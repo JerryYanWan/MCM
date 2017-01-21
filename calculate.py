1acc93ef74af2a18108889e80365594359bbad46
Wan Yan <ywanad@connect.ust.hk>
2017-01-21 21:40:50 +0800

    two scripts to calculate the mean, variance of data from excel and the average queue length for M/M/k model

---
+++ calculate.py
@@ -0,0 +1,39 @@
+import math
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import time
+import argparse
+
+if __name__ == '__main__':
+
+  parser = argparse.ArgumentParser(
+    description = "calculate the average queue length and wait time")
+  parser.add_argument('-k',
+                      default = 1,
+                      type = int,
+                      help = 'number of agents')
+  parser.add_argument('--lamb',
+                      default = 1.,
+                      type = float,
+                      help = 'lambda in M/M/k model')
+  parser.add_argument('--mu',
+                      default = 1.,
+                      type = float,
+                      help = 'mu in M/M/k model')
+  args = parser.parse_args()
+
+  
+  lambda1 = 1.0/float(args.lamb)
+  mu1 = 1.0/float(args.mu)
+  k = args.k
+
+  rho = lambda1/mu1
+  sum1 = np.sum([math.pow(rho, i)/math.factorial(i) for i in np.arange(0, k-1)])
+  P0 = 1.0 / ((k/(k-rho))*(math.pow(rho, k)/ math.factorial(k)) + sum1)
+  
+  L = P0 * (np.sum([math.pow(rho, i)*i/math.factorial(i) for i in np.arange(0, k-1)]) +
+    math.pow(rho, k+1)/(math.factorial(k-1) *math.pow(k-rho, 2)) +
+    math.pow(rho, k)*k/(math.factorial(k-1)*(k-rho)))
+
+  print "P0 = %s, L = %s, W = %s" % (P0, L, L/lambda1)
---
+++ load.py
@@ -0,0 +1,41 @@
+import xlrd
+import sys
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import time
+import argparse
+
+if __name__ == '__main__':
+
+  parser = argparse.ArgumentParser(
+    description = "calculate the mean and variance from excel data")
+  parser.add_argument('--excel',
+                      help = 'load the excel file')
+  parser.add_argument('--diff',
+                      default = 0,
+                      type = int,
+                      help = 'whether to calculate the time stamp or time, 0(time stamp) | 1(time)')
+  parser.add_argument('--col',
+                      default = 0,
+                      type = int,
+                      help = 'columns to process')
+  args = parser.parse_args()
+  df = pd.read_excel(args.excel)
+  date = df[df.columns[args.col]].dropna()
+
+  if args.diff == 0:
+    dateSecond = []
+    print "calculate column %s, %s, diff time stamp" %(args.col, df.columns[args.col])
+    for d in date:
+      dateSecond.append(d.minute*60 + d.second+d.microsecond/1e6)
+    dateSecond = np.array(dateSecond, dtype='float')
+    c = np.diff(dateSecond)
+    print "mean = %s, var = %s" % (c.mean(), c.var())
+  else:
+    dateSecond = []
+    print "calculate column %s, %s, interval time" %(args.col, df.columns[args.col])
+    for d in date:
+      dateSecond.append(d.minute*60 + d.second+d.microsecond/1e6)
+    dateSecond = np.array(dateSecond, dtype='float')
+    print "mean = %s, var = %s" % (dateSecond.mean(), dateSecond.var())
